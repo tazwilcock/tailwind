4b25cad966d0460f16e03e3debae9c2bc975f14e
tazwilcock <tazdevelops@gmail.com>
2023-10-28 13:03:24 -0600

    initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Tailwind Practice
+
+Each folder in the repo focuses on a different aspect of Tailwind CSS.
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html}'],
+	theme: {
+		extend: {
+			fontFamily: {
+			'sans': ['Gabarito', 'sans-serif']
+		},
+		},
+	},
+	plugins: [],
+}
