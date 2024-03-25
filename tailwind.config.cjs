7efd04a5bb9dee5a7116b4b999e010ecc471a614
tazwilcock <tazdevelops@gmail.com>
2024-03-24 19:26:22 -0600

    added flash cards and web forms section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html}'],
-	theme: {
-		extend: {
-			fontFamily: {
-			'sans': ['Gabarito', 'sans-serif'],
-			'title': ['Poiret One', 'sans-serif']
-		},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Red Rose", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
